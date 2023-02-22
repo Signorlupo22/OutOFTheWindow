5be59b16c521ed45ade30aee8ddc77e159312d29
faver <faver@Desktop-Thomas.wind3.hub>
2023-02-22 12:17:09 +0100

    prova

--- ProvaGame/src/Utility/Constants.java
+++ ProvaGame/src/Utility/Constants.java
@@ -1,6 +1,14 @@
 package Utility;
 
+import MainGame.MainGame;
+
 public class Constants {
+	public static class Buttons{
+		public static final int B_WIDTH_DEFAULT = 140;
+		public static final int B_HEIGHT_DEFAULT = 56;
+		public static final int B_WIDTH = (int) (B_WIDTH_DEFAULT * MainGame.SCALE);
+		public static final int B_HEIGHT = (int) (B_HEIGHT_DEFAULT * MainGame.SCALE);
+	}
 	
 	public static class Direciotn{
 		public static final int LEFT = 0;
