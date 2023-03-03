9147be50d004f87d58a83f23a5cb47d432c3c3dc
DarkStram <125917554+DarkStram@users.noreply.github.com>
2023-03-03 13:09:52 +0100

    Add files via upload

---
+++ ProvaGame/src/swingProva/LoadSave.java
@@ -0,0 +1,31 @@
+package swingProva;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class LoadSave {
+	public static final String PLAYER_ATLAS = "Anim.png";
+
+	public static BufferedImage GetSpriteAtlas(String fileName) {
+		BufferedImage img = null;
+		InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+
+		try {
+			img = ImageIO.read(is);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return img;
+
+	}
+}
---
+++ ProvaGame/src/swingProva/Test.java
@@ -0,0 +1,13 @@
+package swingProva;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Menu menu = new Menu();
+		menu.start();
+//		SceltaLivelli l =new SceltaLivelli();
+//		l.start();
+	}
+	
+}
