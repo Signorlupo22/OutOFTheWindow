d6dcc6c7f22968a1cd65be0d4663d09860ecbced
DarkStram <125917554+DarkStram@users.noreply.github.com>
2023-03-10 13:04:32 +0100

    Add files via upload

--- ProvaGame/src/swingProva/GamePanel.java
+++ ProvaGame/src/swingProva/GamePanel.java
@@ -3,10 +3,8 @@
  * 
 */
 
-
-
-
 package swingProva;
+
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -14,51 +12,60 @@ import javax.swing.*;
 
 import Utility.LoadSave;
 
-
 public class GamePanel extends JPanel {
 
 	private boolean moving = false;
+	BufferedImage img = null;
 	
-	public GamePanel() {	
+	public GamePanel() {
 	}
+
 	private void setPanelSize() {
-		Dimension size = new Dimension(400,400);
+		Dimension size = new Dimension(400, 400);
 		setMaximumSize(size);
 		setMinimumSize(size);
 		setPreferredSize(size);
 	}
-	
+
 	public void paintLogoMenu(int x, int y, int width, int height) {
 		super.paintComponent(getGraphics());
-		BufferedImage img = LoadSave.GetSpriteAtlas("LogoConGriglia.png");
+		while (img == null) {
+			img = LoadSave.GetSpriteAtlas("LogoConGriglia.png");
+		}
 		getGraphics().drawImage(img, x, y, width, height, null);
 	}
-	
+
 	public void paintPlay(int x, int y, int width, int height) {
 		super.paintComponent(getGraphics());
-		BufferedImage img = LoadSave.GetSpriteAtlas("play.png");
+		while (img == null) {
+			img = LoadSave.GetSpriteAtlas("play.png");
+		}
 		getGraphics().drawImage(img, x, y, width, height, null);
 	}
+
 	public void paintSceltaLevel(int x, int y, int width, int height, int choose) {
 		super.paintComponent(getGraphics());
-		BufferedImage img = null;
 		
-		//while(img.i) {
-			switch(choose) {
-			case 1:
+		switch (choose) {
+		case 1:
+			while (img == null) {
 				img = LoadSave.GetSpriteAtlas("livello1.png");
-				break;
-			case 2:
-				img = LoadSave.GetSpriteAtlas("livello2.png");
-				break;
-			case 3:
-				img = LoadSave.GetSpriteAtlas("livello3.png");
-				break;
 			}
-		//}
-			
-		
+			break;
+		case 2:
+			while (img == null) {
+				img = LoadSave.GetSpriteAtlas("livello2.png");
+			}
+			break;
+		case 3:
+			while (img == null) {
+				img = LoadSave.GetSpriteAtlas("livello3.png");
+			}
+			break;
+		}
+		// }
+
 		getGraphics().drawImage(img, x, y, width, height, null);
 	}
-	
+
 }
---
+++ ProvaGame/src/swingProva/Setting.java
@@ -0,0 +1,5 @@
+package swingProva;
+
+public class Setting {
+
+}
